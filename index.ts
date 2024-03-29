f4ef04805a12fcb6fd69b0e8ec39dfdc353d1752
faryal16 <162690320+faryal16@users.noreply.github.com>
2024-03-29 06:34:02 +0500

    Add files via upload
    just use your six sence and estimate your number.BEST OF LUCK.

---
+++ index.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber=Math.floor(Math.random() *6+1);
+// console.log(randomNumber);
+const answers=await inquirer.prompt([{
+    name:"userGuessedNumber",
+    type:"number",
+    message:"Please guess a number between 1-6.",
+}])
+if (answers.userGuessedNumber=== randomNumber){
+    console.log("congratulations you guessed right number.")
+}else{
+    console.log("opps you guessed a wrong number.")
+}
